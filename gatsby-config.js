742eb9024b1de460f143ac427738543a22f8e6bb
drew harper <drewharper29@gmail.com>
2022-08-10 10:49:06 -0400

    test file system

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `./src/data/`,
+        path: `./src/images/`,
       },
     },
     `gatsby-transformer-sharp`,
